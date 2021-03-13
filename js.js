7e99e1b993c92ffdda7024eea4eb627874ed50ed
Na1ZeR <74458259+Na1ZeR@users.noreply.github.com>
2021-03-13 12:01:48 +0200

    Add files via upload

---
+++ js.js
@@ -0,0 +1,14 @@
+$(function(){
+    // alert($('h1').text())
+    // alert($('.link-moto').text())
+    // $('.slide-btn').hide()
+    // $('.features-grid').hide(3000)
+    // $('.categorie').show(2000)
+    // alert($('.top-nav li').text())
+    // $('.t ').hide(3500)
+    // $('.t ').show(2000)
+    // $('h3>span').hide(1000)
+    // $('h3>span').show(1000)
+    alert($('.blog-post-info span a').text())
+})
+
